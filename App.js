423b6869cd5ab7a2eb8ca5b1777228d1bf5c65c0
KittinanPog <64070007@kmitl.ac.th>
2023-10-14 15:08:08 +0700

    Update App.js

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "green",
+    backgroundColor: "blue",
     alignItems: "center",
     justifyContent: "center",
   },
