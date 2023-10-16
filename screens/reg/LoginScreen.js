af0eecaaa973f43003ae5e95d3d579b3902dae08
Peak1z <64070073@it.kmitl.ac.th>
2023-10-17 03:26:05 +0700

    Edit CSS

--- screens/reg/LoginScreen.js
+++ screens/reg/LoginScreen.js
@@ -7,17 +7,55 @@ const LoginScreen = ({route, navigation}) => {
 
   return (
     <View style={styles.screen}>
-    <Text>Screen</Text>
-    <Button
-      title="Login"
+      <View style={{ ...{ alignSelf: "left", width: "80%" } }}>
+        <Text style={{ ...styles.text, ...{} }}>ชื่อผู้ใช้</Text>
+      </View>
+      <TextInput
+        style={styles.input}
+        blurOnSubmit
+        autoCapitalize="none"
+        autoCorrect={false}
+        keyboardType="default"
+        // จำนวนตัวอักษรมากสุด
+        maxLength={20}
+        placeholder="ชื่อผู้ใช้"
+        //...เพิ่ม property value และ onChangeText...
+        // value={enteredValue}
+        // onChangeText={numberInputHandler}
+      />
+      <View style={{ ...{ alignSelf: "left", width: "80%" } }}>
+        <Text style={styles.text}>รหัสผ่าน</Text>
+      </View>
+
+      <TextInput
+        style={styles.input}
+        blurOnSubmit
+        autoCapitalize="none"
+        autoCorrect={false}
+        keyboardType="default"
+        // จำนวนตัวอักษรมากสุด
+        maxLength={20}
+        placeholder="รหัสผ่าน"
+        //...เพิ่ม property value และ onChangeText...
+        // value={enteredValue}
+        // onChangeText={numberInputHandler}
+      />
+     <TouchableOpacity style={styles.button}
       onPress={() => {
-        // เขียนโค้ดเพิ่ม
-        navigation.navigate("BottomTabNav", {
-          prev: "Welcome",
-        });
-      }}
-    />
-  </View>
+        navigation.navigate("BottomTabNav");
+      }}>
+        <Text style={{...styles.text,...{alignSelf:"center",}}}>เข้าสู่ระบบ</Text>
+      </TouchableOpacity>
+      <View style={{ ...styles.postRow,...{ alignSelf: "left", width: "80%" ,justifyContent:"center"} }}>
+        <Text style={{...styles.text,...{fontSize:18,color:"blue",marginLeft:20}}}>ยังไม่มีบัญชีใช่ไหม</Text>
+        <TouchableOpacity  onPress={() => {
+        navigation.navigate("Register");
+      }}>
+        <Text style={{...styles.text,...{fontSize:18,marginLeft:10,textDecorationLine:"underline"}}}>สมัครที่นี่</Text>
+
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
--- screens/reg/RegisterScreen.js
+++ screens/reg/RegisterScreen.js
@@ -7,16 +7,124 @@ const RegisterScreen = ({route, navigation}) => {
 
   return (
     <View style={styles.screen}>
-    <Text>RegisterScreen</Text>
-    <Button
-      title="Register"
-      onPress={() => {
-        // เขียนโค้ดเพิ่ม
-        navigation.navigate("Login", {
-          prev: "Login",
-        });
-      }}
+    {/* username */}
+    <View style={{ ...{ alignSelf: "left", width: "80%" } }}>
+      <Text style={{ ...styles.text, ...{} }}>ชื่อผู้ใช้</Text>
+    </View>
+    <TextInput
+      style={styles.input}
+      blurOnSubmit
+      autoCapitalize="none"
+      autoCorrect={false}
+      keyboardType="default"
+      // จำนวนตัวอักษรมากสุด
+      maxLength={20}
+      placeholder="ชื่อผู้ใช้"
+      //...เพิ่ม property value และ onChangeText...
+      // value={enteredValue}
+      // onChangeText={numberInputHandler}
     />
+        {/* email */}
+    <View style={{ ...{ alignSelf: "left", width: "80%" } }}>
+      <Text style={{ ...styles.text, ...{} }}>อีเมล</Text>
+    </View>
+    <TextInput
+      style={styles.input}
+      blurOnSubmit
+      autoCapitalize="none"
+      autoCorrect={false}
+      keyboardType="email-address"
+      // จำนวนตัวอักษรมากสุด
+      maxLength={20}
+      placeholder="อีเมล"
+      //...เพิ่ม property value และ onChangeText...
+      // value={enteredValue}
+      // onChangeText={numberInputHandler}
+    />
+       {/* =ชื่อ */}
+       <View style={{ ...{ alignSelf: "left", width: "80%" } }}>
+      <Text style={{ ...styles.text, ...{} }}>ชื่อจริง</Text>
+    </View>
+    <TextInput
+      style={styles.input}
+      blurOnSubmit
+      autoCapitalize="none"
+      autoCorrect={false}
+      keyboardType="default"
+      // จำนวนตัวอักษรมากสุด
+      maxLength={20}
+      placeholder="ชื่อจริง"
+      //...เพิ่ม property value และ onChangeText...
+      // value={enteredValue}
+      // onChangeText={numberInputHandler}
+    />
+       {/* สกุล*/}
+       <View style={{ ...{ alignSelf: "left", width: "80%" } }}>
+      <Text style={{ ...styles.text, ...{} }}>นามสกุล</Text>
+    </View>
+    <TextInput
+      style={styles.input}
+      blurOnSubmit
+      autoCapitalize="none"
+      autoCorrect={false}
+      keyboardType="default"
+      // จำนวนตัวอักษรมากสุด
+      maxLength={20}
+      placeholder="นามสกุล"
+      //...เพิ่ม property value และ onChangeText...
+      // value={enteredValue}
+      // onChangeText={numberInputHandler}
+    />
+    {/* รหัสผ่าน */}
+    <View style={{ ...{ alignSelf: "left", width: "80%" } }}>
+      <Text style={styles.text}>รหัสผ่าน</Text>
+    </View>
+    <TextInput
+      style={styles.input}
+      blurOnSubmit
+      autoCapitalize="none"
+      autoCorrect={false}
+      keyboardType="password"
+      // จำนวนตัวอักษรมากสุด
+      maxLength={20}
+      placeholder="รหัสผ่าน"
+      //...เพิ่ม property value และ onChangeText...
+      // value={enteredValue}
+      // onChangeText={numberInputHandler}
+    />
+     {/* ยืนยันรหัสผ่าน */}
+     <View style={{ ...{ alignSelf: "left", width: "80%" } }}>
+      <Text style={styles.text}>ยืนยันรหัสผ่าน</Text>
+    </View>
+    <TextInput
+      style={styles.input}
+      blurOnSubmit
+      autoCapitalize="none"
+      autoCorrect={false}
+      keyboardType="default"
+      // จำนวนตัวอักษรมากสุด
+      maxLength={20}
+      placeholder="ยืนยันรหัสผ่าน"
+      //...เพิ่ม property value และ onChangeText...
+      // value={enteredValue}
+      // onChangeText={numberInputHandler}
+    />
+
+   <TouchableOpacity style={styles.button}
+    onPress={() => {
+      navigation.navigate("Login");
+    }}>
+      <Text style={{...styles.text,...{alignSelf:"center",}}}>สมัครสมาชิก</Text>
+    </TouchableOpacity>
+    <View style={{ ...styles.postRow,...{ alignSelf: "left", width: "80%",justifyContent:"center" } }}>
+      <Text style={{...styles.text,...{fontSize:18,color:"blue",marginLeft:20}}}>มีบัญชีแล้ว</Text>
+      <TouchableOpacity  onPress={() => {
+      navigation.navigate("Login");
+    }}>
+      <Text style={{...styles.text,...{fontSize:18,marginLeft:10,textDecorationLine:"underline"}}}>เข้าสู่ระบบที่นี่</Text>
+
+      </TouchableOpacity>
+    </View>
   </View>
   );
 };
