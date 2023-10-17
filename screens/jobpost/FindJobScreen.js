5fc1bfc300768e3701916b98bfaa09702fbe0d04
aummnptp <aumputthipong2002@gmail.com>
2023-10-17 14:00:32 +0700

    Revert "edit dummy"
    This reverts commit 5f41f55bd012686b472bb284beea6fd35835a960.

--- screens/jobpost/FindJobScreen.js
+++ screens/jobpost/FindJobScreen.js
@@ -11,31 +11,6 @@ import {
 } from "react-native";
 
 const FindJobScreen = ({ route, navigation }) => {
-  // const catId = route.params.id;
-  
-  const displayedJobs = useSelector(state => state.jobs.filteredJobs);
-  // const displayedMeals =   availableMeals.filter((meal) => meal.categoryIds.indexOf(catId) >= 0
-  // );
-  const DATA = [
-    {
-      id: "1",
-      Agency: "KMITL",
-      position: "Frontend Dev",
-    },
-    {
-      id: "2",
-      Agency: "KMITL",
-      position: "Frontend Dev",
-    },
-    {
-      id: "3",
-      Agency: "KMITL",
-      position: "Frontend Dev",
-    },
-  ];
-  // const attributeItem = ({}) => 
-  //      (<Text style={styles.detailText}>-มีประสบการณ์5ปีขึ้นไป</Text>);
-  const Item = ({itemData}) => (
   //   const {step, title} = route.params;
   const DATA = [
     {
