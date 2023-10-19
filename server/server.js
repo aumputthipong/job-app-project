e0745be07236efcb629c5776d004118e4cfc0066
aummnptp <aumputthipong2002@gmail.com>
2023-10-19 15:13:34 +0700

    กำลังสร้าง2

--- server/server.js
+++ server/server.js
@@ -4,25 +4,44 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const {db} = require('./database/firebase.js')
+
 
 app.use(bodyParser.json());
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // Folder where the images will be saved
+  destination: (req, file, cb) =>{
+    cb(null, 'Images'); // Folder where the images will be saved
   },
-  filename: function (req, file, cb) {
-    cb(null, new Date().toISOString() + path.extname(file.originalname));
+  filename: (req, file, cb)=> {
+    console.log(file)
+    cb(null, Date.now() + path.extname(file.originalname));
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({ storage: storage });
 
-app.post('/upload', upload.single('image'), (req, res) => {
+app.set("view engine","ejs");
+
+// api
+// อัพรูป
+app.post("/upload", upload.single('image'),(req,res)=>{
+  res.send("Image Uploaded");
+})
+// addPost
+app.post("/addJobPost", async(req,res)=>{
+  const {name,status} = req.body
+  const JobPostRef = db.collection('people').doc('associates')
+  const res = await JobPostRef.set{
+    // [jobTitle]:status
+
+  }
+})
+
+app.get('/api', (req, res) => {
   // Handle the uploaded image, save its information to a database, etc.
-  res.json({ message: 'Image uploaded successfully' });
+  res.json({ "User":["jojo","jojo"] });
 });
-
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
