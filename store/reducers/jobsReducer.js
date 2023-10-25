98499999c3450436f4c9b442153bf679955f8b68
KittinanPog <64070007@kmitl.ac.th>
2023-10-26 03:08:02 +0700

    noti screen

--- store/reducers/jobsReducer.js
+++ store/reducers/jobsReducer.js
@@ -1,45 +1,49 @@
-import { JOBS,FAVORITEJOBS } from "../../data/Jobs-data";
+import { JOBS, FAVORITEJOBS } from "../../data/Jobs-data";
+import { NOTI } from "../../data/Noti-data";
 import { TOGGLE_FAVORITE } from "../actions/jobAction";
 import { LINK_JOB } from "../actions/jobAction";
 import { FILTER_JOBS } from '../actions/jobAction';
-
+import { SET_NEW_POST_AVAILABLE } from '../actions/jobAction';
 import firebase from "../../database/firebaseDB";
 
 
 const initialState = {
     jobs: JOBS,
-    filteredJobs:JOBS ,
+    filteredJobs: JOBS,
     filterJob: JOBS,
-    selectedJob:JOBS[0] ,
-    favoriteJobs: FAVORITEJOBS
-    };
+    selectedJob: JOBS[0],
+    favoriteJobs: FAVORITEJOBS,
+    notiData: NOTI,
+    
+};
 
-    const jobsReducer = (state = initialState, action) => {
-        switch (action.type) {
-            case TOGGLE_FAVORITE:
-                const currentUserId = firebase.auth().currentUser.uid;
-                const updatedFavoriteJobs = [...state.favoriteJobs];
-                const favIndex = updatedFavoriteJobs.findIndex((job) => job.postId === action.jobId && job.userId === currentUserId);
-            
-                if (favIndex === -1) {
-          
-                    updatedFavoriteJobs.push( {postId:action.jobId,userId:currentUserId});
-                    // console.log('push', updatedFavoriteJobs);
-                    // console.log(updatedFavoriteJobs);
-                    firebase.firestore().collection("FavoriteJobs").add({
-                        postId: action.jobId,
-                        userId: currentUserId
-                    })
+const jobsReducer = (state = initialState, action) => {
+    switch (action.type) {
+
+        case TOGGLE_FAVORITE:
+            const currentUserId = firebase.auth().currentUser.uid;
+            const updatedFavoriteJobs = [...state.favoriteJobs];
+            const favIndex = updatedFavoriteJobs.findIndex((job) => job.postId === action.jobId && job.userId === currentUserId);
+
+            if (favIndex === -1) {
+
+                updatedFavoriteJobs.push({ postId: action.jobId, userId: currentUserId });
+                // console.log('push', updatedFavoriteJobs);
+                // console.log(updatedFavoriteJobs);
+                firebase.firestore().collection("FavoriteJobs").add({
+                    postId: action.jobId,
+                    userId: currentUserId
+                })
                     .then(() => {
                         console.log('Job added to favorites on Firebase');
                     })
                     .catch((error) => {
                         console.error('Error adding job to favorites: ', error);
                     });
-                }
-                else {
-                    updatedFavoriteJobs.splice(favIndex, 1);
-                    firebase.firestore().collection("FavoriteJobs")
+            }
+            else {
+                updatedFavoriteJobs.splice(favIndex, 1);
+                firebase.firestore().collection("FavoriteJobs")
                     .where("postId", "==", action.jobId)
                     .where("userId", "==", currentUserId)
                     .get()
@@ -55,38 +59,80 @@ const initialState = {
                     .catch((error) => {
                         console.error('Error removing job from favorites: ', error);
                     });
-                   
-                }
-                console.log(  updatedFavoriteJobs)
-                return {
-                    ...state,
-                    favoriteJobs: updatedFavoriteJobs, 
-                };
-                
-                case LINK_JOB:
-                
-                // const selectedJob = [...state.favoriteJobs];
-                case FILTER_JOBS:
 
-                const { selected } = action;
-                    if (selected.length === 0) {
-                        // ถ้าไม่มีค่าที่ถูกเลือก ให้แสดงทั้งหมด
-                        return { ...state, filterJob: state.jobs };
-                      }
-                    
-                      // ดำเนินการกรองโพสต์ตามค่าที่ถูกเลือก
-                      const filteredJobs = state.jobs.filter((job) => {
-                        return selected.includes(job.category);
-                      });
-                      
-                      return { ...state, filterJob: filteredJobs };
-                
+            }
+            console.log(updatedFavoriteJobs)
+            return {
+                ...state,
+                favoriteJobs: updatedFavoriteJobs,
+            };
 
-    
-            default:
-                return state;
-        }
+        case LINK_JOB:
+
+        // const selectedJob = [...state.favoriteJobs];
+        case FILTER_JOBS:
+            const { selected } = action;
+            const userId = firebase.auth().currentUser.uid;
+            const updatedNoti = [...state.notiData];
+            const notiIndex = updatedNoti.findIndex((noti) => noti.notiBy === userId);
+
+            if (notiIndex === -1) {
+                updatedNoti.push({ category: selected, notiBy: userId });
+                firebase.firestore().collection("User Noti").add({
+                    category: selected,
+                    notiBy: userId
+                })
+                    .then((docRef) => {
+                        const docId = docRef.id;
+                        updatedNoti.push({ category: selected, notiBy: userId, docId: docId });
+                        return {
+                            ...state,
+                            notiData: updatedNoti
+                        };
+                    })
+                    .then(() => {
+                        console.log('Notification added to Noti on Firebase');
+                    })
+                    .catch((error) => {
+                        console.error('Error adding notification to Noti: ', error);
+                    });
+            } else {
+                const docId = updatedNoti[notiIndex].id;
+                firebase.firestore().collection("User Noti").doc(docId).update({
+                    category: selected,
+                    notiBy: userId
+                })
+                    .then(() => {
+                        console.log("Notification updated in Noti on Firebase");
+                    })
+                    .catch((error) => {
+                        console.error("Error updating notification in Noti: ", error);
+                    });
+                const updatedNotiData = [...updatedNoti];
+                updatedNotiData[notiIndex].category = selected;
+            }
+
+
+            // เมื่ออัปเดตข้อมูลแล้วคุณควรอัปเดต local state เช่น notiData
+            if (selected.length === 0) {
+                // ถ้าไม่มีค่าที่ถูกเลือก ให้แสดงทั้งหมด
+                return { ...state, filterJob: state.jobs };
+              }
+            
+              // ดำเนินการกรองโพสต์ตามค่าที่ถูกเลือก
+              const filteredJobs = state.jobs.filter((job) => {
+                return selected.includes(job.category);
+              });
+              
+              return { ...state, filterJob: filteredJobs };
+           
+
         
+        // ... กรณีอื่น ๆ
+        default:
+            return state;
     }
-    
-    export default jobsReducer;
+
+}
+
+export default jobsReducer;
